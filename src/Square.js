ca4470832ffa21fa72fc435dffcad3849f0a5689
Will Farley <wfarley@amazon.com>
2023-05-09 14:03:17 -0700

    Refactor square

---
+++ src/Square.js
@@ -0,0 +1,7 @@
+import styles from './Square.module.css'
+
+export const Square = ({value, onSquareClick }) => (
+    <button className={styles.Square} onClick={onSquareClick}>
+      {value}
+    </button>
+)
